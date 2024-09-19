f35aae7a381accddef6ef87d2346be1d3739ec75
Michael Wells <info@sygnal.com>
2024-09-19 20:14:19 +0000

    Posthog

---
+++ dist/components/test.js
@@ -0,0 +1,15 @@
+"use strict";
+(() => {
+  // src/components/test.ts
+  var TestComponent = class {
+    constructor(elem) {
+      this.elem = elem;
+    }
+    setup() {
+    }
+    exec() {
+      const sa5 = window["sa5"];
+    }
+  };
+})();
+//# sourceMappingURL=test.js.map
